c3644f5ec01933eaf45cf25a07f55e767f45212d
alanbabu43 <alanbabu505@gmail.com>
2026-03-17 21:48:33 +0530

    secrect key

--- chat_system/chat_system/settings.py
+++ chat_system/chat_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-whvtg-6r@f^gfst+9_i+_d-d8_)nz=qw%)p_#^)%!_4uot%@+c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
